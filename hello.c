ab1463161372b80bf8f0741dc52a220965243f9b
Thanassis Tsiodras <ttsiodras@gmail.com>
2020-08-08 23:44:52 +0200

    Precompute everything.

--- hello.c
+++ hello.c
@@ -13,16 +13,7 @@
 #define printPaper(k)        printf("\x11%c", '0'+(k))
 #define printAt(row, col)    printf("\x16%c%c", (col)+1, (row)+1)
 
-// The angle of rotation in the Z-axis. Goes from 0 up to 71 for a full circle
-// (see lookup table inside sincos.h).
-static int angle = 0;
-
-static int msin = 0, mcos = 0;
-static int msin_old = 0, mcos_old = 0;
-
-const int Se = 16*16 + maxx/16;
-const int SCREEN_DISTX = 64;
-const int SCREEN_DISTY = 32;
+#define TOTAL_FRAMES 36
 
 void cls()
 {
@@ -30,21 +21,27 @@ void cls()
     gotoxy(0,0);
 }
 
+struct {
+    int x;
+    int y;
+} precomputed[TOTAL_FRAMES][sizeof(points)/sizeof(points[0])];
+
 ///////////////////////////////////////////////////////////////
 // 3D projection - make a diagram or read any 3D graphics book.
 ///////////////////////////////////////////////////////////////
 
-void drawPoints()
+void precomputePoints(int angle)
 {
-    static int old_xx[sizeof(points)/sizeof(points[0])];
-    static int old_yy[sizeof(points)/sizeof(points[0])];
-
+    const int Se = 16*16 + maxx/16;
+    const int SCREEN_DISTX = 64;
+    const int SCREEN_DISTY = 32;
     const int width=256;
     const int height=192;
-    for(unsigned pt=0; pt<sizeof(points)/sizeof(points[0]); pt++) {
 
-        // Clear old pixel
-        unplot(old_xx[pt], old_yy[pt]);
+    int msin = sincos[2*angle].si;
+    int mcos = sincos[2*angle].co;
+
+    for(unsigned pt=0; pt<sizeof(points)/sizeof(points[0]); pt++) {
 
         // Read the statue data.
         int wx = points[pt][0];
@@ -54,15 +51,23 @@ void drawPoints()
         // Now that we read the X,Y,Z data, project them to 2D
         int wxnew = wx+mcos; // (mcos*wx - msin*wy)/256L;
         int wynew = wy+msin; // (msin*wx + mcos*wy);
-        int x = width/2L + ((wynew << 6)/(Se-wxnew));
-        int y = height/2L - ((wz << 6)/(Se-wxnew));
+        precomputed[angle][pt].x = width/2L + ((wynew << 6)/(Se-wxnew));
+        precomputed[angle][pt].y = height/2L - ((wz << 6)/(Se-wxnew));
+    }
+}
+
+void drawPoints(int angle)
+{
+    const int width=256;
+    const int height=192;
+    int old_angle = angle ? (angle - 1) : (TOTAL_FRAMES - 1);
+
+    for(unsigned pt=0; pt<sizeof(points)/sizeof(points[0]); pt++) {
+        // Clear old pixel
+        unplot(precomputed[old_angle][pt].x, precomputed[old_angle][pt].y);
 
         // Set new pixel
-        plot(x, y);
-
-        // Remember new pixel to be able to clear it in the next frame
-        old_xx[pt] = x;
-        old_yy[pt] = y;
+        plot(precomputed[angle][pt].x, precomputed[angle][pt].y);
     }
 }
 
@@ -70,6 +75,7 @@ main()
 {
     long frames = 0;
     long m = 0, st, en;
+    unsigned char angle;
 
     cls();
     zx_border(INK_BLACK);
@@ -86,34 +92,37 @@ main()
         sincos[pt].si /= 4;
         sincos[pt].co /= 4;
     }
-    printf("[-] Rendering...\n");
-    printf("[-] Q to quit...\n");
 #ifdef MANUAL_CONTROL
     uint oo = in_LookupKey('o');
     uint pp = in_LookupKey('p');
 #endif
+    printf("[-] Precomputing");
     uint qq = in_LookupKey('q');
+    for(angle=0; angle<TOTAL_FRAMES; angle++) {
+        printf(".");
+        precomputePoints(angle);
+    }
+    cls();
+    printf("[-] Rendering...\n");
+    printf("[-] Q to quit...\n");
+    angle = 0;
     st = clock();
     //while(frames<32) {
     while(1) {
-        // Rotate by 5 degrees on each iteration (360/72)
-        angle = frames%72L;
-        // Recompute sin/cos from the lookup table
-        msin = sincos[angle].si;
-        mcos = sincos[angle].co;
-        drawPoints();
+        drawPoints(angle);
 #ifdef MANUAL_CONTROL
         if (in_KeyPressed(oo))
-            frames = (frames + 71)%72;
+            frames = (frames + TOTAL_FRAMES - 1)%TOTAL_FRAMES;
         else if (in_KeyPressed(pp))
-            frames = (frames + 1)%72;
+            frames = (frames + 1)%TOTAL_FRAMES;
 #else
         frames++;
+        angle++;
+        if (angle == TOTAL_FRAMES)
+            angle = 0;
         if (in_KeyPressed(qq))
             break;
 #endif
-        msin_old = msin;
-        mcos_old = mcos;
     }
     en = clock();
     cls();
